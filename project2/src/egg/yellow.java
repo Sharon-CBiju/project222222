1b199ffbd98c43b54e45c72983e8caa46346d093
Sharon-CBiju <147945041+Sharon-CBiju@users.noreply.github.com>
2024-03-14 18:38:19 +0000

    Update yellow.java

--- project2/src/egg/yellow.java
+++ project2/src/egg/yellow.java
@@ -4,6 +4,7 @@ public class yellow {
 	public static void main(String[] args) {
 		System.out.println("good fat");
 		System.out.println("push tyo github after fork");
+		System.out.println("edit in github to learn pull");
 	}
 
 }
